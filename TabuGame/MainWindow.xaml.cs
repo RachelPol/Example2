1d6c18c46ab51ba660cec10ec79b097d075c8aa6
Rachel Polak <rachelpolakw@gmail.com>
2022-01-24 11:15:31 +0200

    First commit

---
+++ TabuGame/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TabuGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TabuGameVM tabuGameVm;
+        public MainWindow()
+        {
+            var settings = new Settings();
+            settings.ShowDialog();
+            int number = 40;
+            int.TryParse(settings.NumberOfCardsTextBox.Text, out number);
+            tabuGameVm = new TabuGameVM(2, 40, 5);
+            
+            DataContext = tabuGameVm;
+            InitializeComponent();
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var b = sender as Button;
+            var g = b.DataContext as Group;
+            g.AddCard();
+
+            tabuGameVm.CardDecrease();
+            
+        }
+        
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            tabuGameVm.StartTimer();
+        }
+    }
+}
---
+++ TabuGame/TabuGameVM.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+
+namespace TabuGame
+{
+    public class TabuGameVM:INotifyPropertyChanged
+    {
+        System.Timers.Timer recordTimer = new System.Timers.Timer(1000);
+        public TabuGameVM(int numberOfGroups, int numberOfCards, int numberOfExchanges)
+        {
+            recordTimer.Elapsed += RecordTimer_Elapsed;
+            numberOfCardsInTheStack = numberOfCards;
+            ListOfGroups = new List<Group>();
+            for (int i = 0; i < numberOfGroups; i++)
+            {
+                ListOfGroups.Add(new Group(i + 1));
+            }
+        }
+
+        private void RecordTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            if (numberOfSecondStays > 0)
+            {
+                numberOfSecondStays--;
+                NotifyPropertyChanged("NumberOfSecondsStays");
+            }
+            else
+            {
+                recordTimer.Stop();
+            }
+        }
+        private int numberOfSecondStays;
+
+        public string NumberOfSecondsStays
+        {
+            get { return numberOfSecondStays.ToString(); }
+        }
+
+        internal void StartTimer()
+        {
+            numberOfSecondStays = 60;
+            recordTimer.Start();
+        }
+
+        private int numberOfCardsInTheStack;
+
+        public string CardsStay
+        {
+            get { return numberOfCardsInTheStack.ToString(); }
+        }
+
+        public List<Group> ListOfGroups { get; set; }
+
+        internal void CardDecrease()
+        {
+            numberOfCardsInTheStack--;
+            NotifyPropertyChanged("CardsStay");
+        }
+
+        private void NotifyPropertyChanged(string pProperty)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(pProperty));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+    public class Group: INotifyPropertyChanged
+    {
+        public Group(int number)
+        {
+            numberOfCards = 0;
+            GroupTitle = "קבוצה " + number.ToString();
+            //GetNewCardCommand = new RelayCommand(() => { numberOfCards++; });
+        }
+        public string GroupTitle { get; set; }
+        private int numberOfCards;
+
+        public ICommand GetNewCardCommand { get; set; }
+        public string NumberOfCards
+        {
+            get { return numberOfCards.ToString(); }
+            
+        }
+
+        internal void AddCard()
+        {
+            numberOfCards++;
+            NotifyPropertyChanged("NumberOfCards");
+        }
+        private void NotifyPropertyChanged(string pProperty)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(pProperty));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    }
+}
